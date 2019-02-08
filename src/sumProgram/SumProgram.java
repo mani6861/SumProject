7e714e8445a357313916811c766bb253082f045f
mani6861 <mani6861@gmail.com>
2019-02-08 20:31:57 +0000

    first commit

---
+++ src/sumProgram/SumProgram.java
@@ -0,0 +1,23 @@
+package sumProgram;
+
+public class SumProgram {
+
+
+
+    public static void main(String[] args) {
+
+        int a = 125;
+        int b = 24;
+
+        System.out.println("Sum of 2 numbers = "+(a+b));
+        System.out.println("Difference of 2 numbers = "+(a-b));
+        System.out.println("Multiplication of 2 numbers = "+(a*b));
+        System.out.println("Division of 2 numbers = "+(a/b));
+        System.out.println("Remainder of 2 numbers = "+(a % b));
+
+
+
+
+
+    }
+}
